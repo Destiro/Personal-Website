b7aa93970b9a1c5b737692db02622835568c4746
Connor de Bruyn <connordebruyn234@gmail.com>
2022-08-14 09:32:04 +1200

    Added a colour config file

---
+++ src/config/Colours.js
@@ -0,0 +1,18 @@
+/**
+ * Colours that I will be using throughout the project
+ */
+export default {
+    //Colour Pallet
+    red_primary: '#e61f05',
+    red_secondary: '#ad1c02',
+    yellow_primary: '#fcba03',
+    yellow_secondary: '#b5a002',
+    green_primary: '#35c20a',
+    green_secondary: '#1c7002',
+
+    //Standard Colours
+    black: '#000',
+    white: '#fff',
+    light_grey: '#dedede',
+    dark_grey: '#888888',
+}
