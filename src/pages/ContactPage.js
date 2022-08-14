38037d1f01133870628e63ab34142a409513c8f1
Connor de Bruyn <connordebruyn234@gmail.com>
2022-08-14 12:33:49 +1200

    Added nav bar to all pages

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
+import NavBar from "../assets/NavBar";
 
 const contactPage = () => {
     return (
         <View>
+            <NavBar />
             <h1>Contact Page</h1>
         </View>
     );
--- src/pages/ExperiencePage.js
+++ src/pages/ExperiencePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
+import NavBar from "../assets/NavBar";
 
 const experiencePage = () => {
     return (
         <View>
+            <NavBar />
             <h1>Experience Page</h1>
         </View>
     );
